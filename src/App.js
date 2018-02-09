f99c1aaaa6607b0dfa780186fcc34a9499db6163
liebeskind <daniel.liebeskind@gmail.com>
2018-02-08 21:29:30 -0800

    Now console logs the account retrieved by web3.eth.getAccounts().

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import web3 from './web3';
 
 class App extends Component {
   render() {
-    console.log(web3.version);
-    
+    web3.eth.getAccounts()
+      .then(console.log);
+
     return (
       <div className="App">
         <header className="App-header">
