7753ae9d2ac13a9148341598934d91dad8773c15
liebeskind <daniel.liebeskind@gmail.com>
2018-02-08 23:21:58 -0800

    Builds out lottery.methods.enter functionality

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
   state = { 
     manager: '',
     players: [],
-    balance: ''
+    balance: '',
+    value: ''
   }
 
   async componentDidMount() {
@@ -18,6 +19,17 @@ class App extends Component {
     const balance = await web3.eth.getBalance(lottery.options.address); //Big number object
 
     this.setState({ manager, players, balance });
+  }
+
+  onSubmit = async (event) => { //Binds this to our component
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    await lottery.methods.enter().send({ 
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    })
   }
 
   render() {
@@ -29,6 +41,19 @@ class App extends Component {
         There are currently {this.state.players.length} people entered, 
         competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value} 
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
